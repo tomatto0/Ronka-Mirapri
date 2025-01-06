b84c43fb88369599e94dd837594c2c520a497fd6
neulrain <verumnium@gmail.com>
2025-01-06 20:36:04 +0900

    typescript downScale, UserCanvas 주석추가

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,10 +1,12 @@
 export default function ItemUploader({ set_image_src }: 
     {set_image_src: (src: string) => void}) {
     const image_validate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // 입력된 파일의 확장자 추출
         const ext = e.target.value.substring(
-            e.target.value.lastIndexOf('.') +1,
+            e.target.value.lastIndexOf('.') + 1,
             e.target.value.length
         ).toLowerCase();
+
         if (['bmp', 'png', 'jpeg', 'jpg'].includes(ext) && e.target.files !== null) {
             const reader = new FileReader();
             reader.readAsDataURL(e.target.files[0]);

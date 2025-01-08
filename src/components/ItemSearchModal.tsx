96775f7bc2147dc5e51b95c61e04e52efcea1452
ParkGeon <cdtd0516@gmail.com>
2025-01-08 21:35:32 +0900

    스타일 카탈로그 전용 팔레트 색 선택 취소 기본색(테레빈유) 아이템 선택 취소

--- src/components/ItemSearchModal.tsx
+++ src/components/ItemSearchModal.tsx
@@ -53,6 +53,7 @@ export default function ItemSearchModal({
   const modal_close = () => {
     set_is_open(false);
     set_search_result([]);
+    set_keyword("");
     // set_is_item_select(false);
   };
 
@@ -71,7 +72,9 @@ export default function ItemSearchModal({
         }}
       >
         <span>{slots[slot]}</span>
-
+        <button className="image" onClick={modal_close}>
+          <img src="./img/cancle.svg" alt="cancle button" />
+        </button>
         <ItemSearch
           keyword={keyword}
           set_keyword={set_keyword}

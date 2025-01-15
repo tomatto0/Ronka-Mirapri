1918079d9820edf86be02df76c289e914f9e88ee
ParkGeon <cdtd0516@gmail.com>
2025-01-15 15:42:54 +0900

    패션 소품 (우산, 날개, 가방) 추가

--- src/ronka_data_crawler.py
+++ src/ronka_data_crawler.py
@@ -7,13 +7,15 @@ raw_data = pd.read_csv('./src/csv/item.csv', header=1, index_col=0, low_memory=F
 raw_data.drop(['int32', '0'], axis=0, inplace=True)
 
 #필요한 행(이름, 아이콘, 염색 수, ...)만 잘라서 items로 저장
-items = raw_data[['Singular', 'Icon', 'DyeCount', 'ClassJobCategory', 'EquipSlotCategory']].copy()
+items = raw_data[['Singular', 'Icon', 'DyeCount', 'ClassJobCategory', 'EquipSlotCategory', 'ItemSortCategory', 'Unnamed: 20']].copy()
 items.index = pd.to_numeric(items.index)
 items['Singular'] = items['Singular'].astype('string')
 items['Icon'] = items['Icon'].astype('string')
 items['DyeCount'] = pd.to_numeric(items['DyeCount'])
 items['ClassJobCategory'] = pd.to_numeric(items['ClassJobCategory'])
 items['EquipSlotCategory'] = pd.to_numeric(items['EquipSlotCategory'])
+items['ItemSortCategory'] = pd.to_numeric(items['ItemSortCategory'])
+items['Unnamed: 20'] = pd.to_numeric(items['Unnamed: 20'])
 
 #착용가능한(equip slot category이 지정됨 + class job category가 지정됨) 아이템 필터링
 equipable_items = items[(items['EquipSlotCategory'] != 0) & (items['ClassJobCategory'] != 0)].copy()
@@ -34,8 +36,13 @@ style_items.loc[style_items.index, 'DyeCount'] = 1
 style_items.loc[style_items.index, 'ClassJobCategory'] = 1
 style_items.loc[style_items.index, 'EquipSlotCategory'] = 24
 
+#패션 소품 아이템 필터링
+fashion_items = items[(items['Unnamed: 20'].isin([30120, 30140, 30180])) & (items['ItemSortCategory'] == 59)]
+fashion_items.loc[:, 'ClassJobCategory'] = 1
+fashion_items.loc[:, 'EquipSlotCategory'] = 24
+
 #착용가능한 아이템 + 스타일카탈로그를 합친 데이터
-filtered_items = pd.concat([equipable_items, style_items])
+filtered_items = pd.concat([equipable_items, style_items, fashion_items])
 
 #아이콘 넘버링을 경로로 수정
 filtered_items.Icon = '/i/0'+filtered_items.Icon.str[:2]+'000/0'+filtered_items.Icon+'_hr1.png'
@@ -45,6 +52,7 @@ filtered_items.drop(axis=0, index=filtered_items[filtered_items.Singular.str.con
 
 #index를 id로, 컬럼명 수정
 filtered_items.reset_index(inplace=True)
+filtered_items.drop(['ItemSortCategory', 'Unnamed: 20'], axis=1, inplace=True)
 filtered_items.columns = ['Id', 'Name', 'Icon', 'DyeCount', 'ClassJobCategory', 'EquipSlotCategory']
 
 #json으로 저장
